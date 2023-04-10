5eb468c0884f5cb6f90e8a92c308180662fab27f
Petteri Mattila <petterimattila4@outlook.com>
2023-04-10 15:45:14 +0300

    12.2. completed

--- 12 Build a Command Line Program/command_line_project/src/main.rs
+++ 12 Build a Command Line Program/command_line_project/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     // get the arguments that were passed through command line
@@ -7,6 +8,12 @@ fn main() {
     let query = &args[1];
     let file_path = &args[2];
 
-    println!("Searching for {}", query);
     println!("In file {}", file_path);
+    println!("Searching for {}", query);
+
+    // Read a file
+    let contents = fs::read_to_string(file_path)
+        .expect("Error occured while reading a file");
+
+    println!("Contents of the file:\n{}", contents);
 }
