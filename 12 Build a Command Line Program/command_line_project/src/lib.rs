7af944a9136b85841dd7bf96475aeb6c0ebb503f
Petteri Mattila <petterimattila4@outlook.com>
2023-04-17 11:14:46 +0300

    12.5. completed + structure changes

--- 12 Build a Command Line Program/command_line_project/src/lib.rs
+++ 12 Build a Command Line Program/command_line_project/src/lib.rs
@@ -1,9 +1,11 @@
 use std::error::Error;
 use std::fs;
+use std::env;
 
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -20,8 +22,12 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
 
+        // Ignore case variable is populated through IGNORE_CASE variable,
+        // which can be passed by giving argument "IGNORE_CASE=1".
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
         // and because the function returns Result Config needs to be wrapped.
-        Ok(Config { query, file_path })
+        Ok(Config { query, file_path, ignore_case })
     }
 }
 
@@ -31,7 +37,13 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // ? will return the error value from the current function for the caller to handle.
     let contents = fs::read_to_string(config.file_path)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{line}");
     }
 
@@ -53,6 +65,20 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        // Works as intended, but will break with certain unicodes 
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
 // Old implementation, implemented in Config struct
 // For this sized project, separating parse functionality like this might be too much.
 // fn parse_config(args: &[String]) -> Config {
@@ -66,3 +92,34 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 //     // Chapter 13 introduces more efficient to solve the ownership problem.
 //     Config { query, file_path }
 // }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+            Rust:
+            safe, fast, productive.
+            Pick three.
+            Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+            Rust:
+            safe, fast, productive.
+            Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
